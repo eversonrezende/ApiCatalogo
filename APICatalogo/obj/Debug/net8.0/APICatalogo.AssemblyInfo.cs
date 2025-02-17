224c241df9112fd1319121fb03628aa23aae3c9a
Everson Rezende <rezende.everson@gmail.com>
2025-02-17 14:54:23 -0300

    add paged list async

--- APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d662807cac61aca5b8b518d25f9e393f7b16a37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c57c6b4c71305d343a29f8fd2637e1122ce0ac7")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
